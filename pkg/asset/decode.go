ab2406090127d8704469d597bdb1924d672e4996
bitxeno <137328844+bitxeno@users.noreply.github.com>
2023-07-01 15:21:17 +0800

    Fix out of memory

--- pkg/asset/decode.go
+++ pkg/asset/decode.go
@@ -268,9 +268,8 @@ func (a *asset) Renditions(loop func(cb *RenditionCallback) (stop bool)) error {
 					return err
 				}
 			}
-		case "PDF":
 		default:
-			return fmt.Errorf("unknown rendition with pixel format: %v", c.PixelFormat.String())
+			log.Printf("unknown rendition with pixel format: %v", c.PixelFormat.String())
 		}
 
 		return nil
